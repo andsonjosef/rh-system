0319e91b740e1cf8a6054f788475033036404287
andsonjosef <andson.josef@gmail.com>
2019-05-02 18:45:24 +0000

    Jobs: fixes inside controller and notifications

--- app/controllers/admins_backoffice/jobs_controller.rb
+++ app/controllers/admins_backoffice/jobs_controller.rb
@@ -1,19 +1,28 @@
 class AdminsBackoffice::JobsController < AdminsBackofficeController
+  before_action :set_job, only: [:edit, :update]
+  
   def index
     @jobs = Job.all
   end
 
   def edit 
-    @job = Job.find(params[:id])
+
   end
 
   def update 
-    job = Job.find(params[:id])
-    params_job = params.require(:job).permit(:title, :description, :local, :salary, :employment_type, :status)
-    if job.update(params_job)
+    if @job.update(params_job)
       redirect_to admins_backoffice_jobs_path, notice: "Vaga atualizada"
     else
       render :edit
     end
+  end
+
+  private
+  def params_job
+    params.require(:job).permit(:title, :description, :local, :salary, :employment_type, :status)
+  end
+
+   def set_job
+    @job = Job.find(params[:id])
   end
 end
--- db/migrate/20190428203408_create_jobs.rb
+++ db/migrate/20190428203408_create_jobs.rb
@@ -1,12 +1,12 @@
 class CreateJobs < ActiveRecord::Migration[5.2]
   def change
     create_table :jobs do |t|
-      t.string :title
-      t.text :description
-      t.string :local
-      t.string :salary
-      t.string :employment_type
-      t.string :status
+      t.string :title, null: false
+      t.text :description, null: false
+      t.string :local, null: false
+      t.string :salary, null: false
+      t.string :employment_type, null: false
+      t.string :status, null: false
 
       t.timestamps
     end
